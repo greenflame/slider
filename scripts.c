4041d91bdcabe0eda33ac110f681a1eab3f52071
Александр Колесов <Alexander@Aleksandrs-MacBook-Air.local>
2016-01-08 17:39:58 +0300

    scripts refactoring

--- scripts.c
+++ scripts.c
@@ -15,6 +15,10 @@
 #include "camera.h"
 
 #define SLEEP_INTERVAL          10      // ticks
+#define AXIS_COUNT              2
+#define COORDINATES_COUNT       (AXIS_COUNT + 1)
+#define SCRIPT_MAX_LENGTH       128
+#define SPLITTERS               "[;]"
 
 /*
  * For recursion
@@ -27,8 +31,69 @@ float parseGroup(char **str, float n);
  */
 float solveExpr(char *expr, float n)
 {
-    char *expr_copy = expr;
-    return parseAdd(&expr_copy, n);
+    char *exprCopy = expr;
+    return parseAdd(&exprCopy, n);
+}
+
+/*
+ * Extract step count from script
+ */
+int solveSteps(char *script)    // check
+{
+    char scriptCpy[128];
+    strcpy(scriptCpy, script);
+    
+    return atoi(strtok(scriptCpy, SPLITTERS));
+}
+
+/*
+ * Solve coorginates(time, axis positions) fo current step
+ */
+void solveCoordinates(char *script, int step, float *coordinates, int coordinatesCount)
+{
+    char scriptCpy[128];
+    strcpy(scriptCpy, script);
+    
+    strtok(scriptCpy, SPLITTERS);   // Ignore steps count
+    
+    for (int i = 0; i < coordinatesCount; i++)
+    {
+        char *curCoordinate = strtok(NULL, SPLITTERS);
+        coordinates[i] = solveExpr(curCoordinate, step);
+    }
+}
+
+// int main()
+// {
+//     printf("hello!\n");
+    
+//     char script[128];
+//     gets(script);
+    
+//     int steps = solveSteps(script);
+//     printf("%d\n", steps);
+    
+//     for (int i = 0; i < steps; i++)
+//     {
+//         float coordinates[COORDINATES_COUNT];
+//         solveCoordinates(script, i, coordinates, COORDINATES_COUNT);
+//         for (int j = 0; j < COORDINATES_COUNT; j++)
+//         {
+//             if (j) printf(" ");
+//             printf("%f", coordinates[j]);
+//         }
+//         printf("\n");
+//     }
+    
+//     return 0;
+// }
+
+/*
+ * Print float as int
+ */
+int ftoi(float in)
+{
+    return (int)(in * 1000);
 }
 
 /*
@@ -38,43 +103,45 @@ int scriptsCheck(char *script, BaseSequentialStream *out)
 {
     chprintf(out, "Checking script...\r\n");
 
-    // Local copy(split corrupts string)
-    char script_cpy[128];
-    strcpy(script_cpy, script);
-
-    // Splitting expression
-    int shots = atoi(strtok(script_cpy, "[;]"));
-    char *time_expr = strtok(NULL, "[;]");
-    char *axis0_expr = strtok(NULL, "[;]");
+    int shots = solveSteps(script);
 
     int i;
     for (i = 1; i < shots; i++)
     {
         // Calculating coordinates
-        float prev_time  = solveExpr(time_expr, i - 1);
-        float prev_axis0 = solveExpr(axis0_expr, i - 1);
+        float *prevCoordinates[COORDINATES_COUNT];
+        float *curCoordinates[COORDINATES_COUNT];
+        
+        solveCoordinates(script, i - 1, prevCoordinates, COORDINATES_COUNT);
+        solveCoordinates(script, i, curCoordinates, COORDINATES_COUNT);
 
-        float cur_time = solveExpr(time_expr, i);
-        float cur_axis0 = solveExpr(axis0_expr, i);
-
-        float axis0_move_time = axisGetMotionTime(AXIS0, prev_axis0, cur_axis0);
+        // Calculating max motion time
+        float maxMotionTime = 0;
+        int slowestAxis = -1;
+        for (int j = 0; j < AXIS_COUNT; j++)
+        {
+            float curAxisMotionTime = axisGetMotionTime(j, prevCoordinates[j + 1], curCoordinate[j + 1]);
+                
+            if (curAxisMotionTime > maxMotionTime)
+            {
+                maxMotionTime = max(maxMotionTime, curAxisMotionTime);
+                slowestAxis = j;
+            }
+        }
 
         // Check time correctness
-        if (cur_time - prev_time < axis0_move_time + cameraGetExposure() + cameraGetStabilizationTime())  // axis0
+        if (cur_time - prevTime < maxMotionTime + cameraGetExposure() + cameraGetStabilizationTime())
         {
-            chprintf(out, "Axis1 cann't be in time.\r\n\r\n");
+            chprintf(out, "Axis %d cann't be in time at step %d.\r\n\r\n", slowestAxis, i);
 
-            chprintf(out, "Previous step: %d, time: %d, pos: %d\r\n", i - 1, (int)(prev_time * 1000), (int)(prev_axis0 * 1000));
-            chprintf(out, "Current step: %d, time: %d, pos: %d\r\n\r\n", i, (int)(cur_time * 1000), (int)(cur_axis0 * 1000));
+            chprintf(out, "Free time: %d ms\r\n", ftoi(cur_time - prevTime));
+            chprintf(out, "Motion time: %d ms\r\n", ftoi(maxMotionTime));
+            chprintf(out, "Shot time: %d ms\r\n", ftoi(cameraGetExposure()));
+            chprintf(out, "Stabilization time: %d ms\r\n", ftoi(cameraGetStabilizationTime());
+            chprintf(out, "Time limit: %d ms\r\n", ftoi(prevCoordinates[0] - curCoordinates[0]
+                + maxMotionTime + cameraGetExposure() + cameraGetStabilizationTime()));
 
-
-            chprintf(out, "Free time: %d ms\r\n", (int)((cur_time - prev_time) * 1000));
-            chprintf(out, "Move time: %d ms\r\n", (int)(axis0_move_time * 1000));
-            chprintf(out, "Shot time: %d ms\r\n", (int)(cameraGetExposure() * 1000));
-            chprintf(out, "Stabilization time: %d ms\r\n", (int)(cameraGetStabilizationTime() * 1000));
-            chprintf(out, "Time defect: %d ms\r\n", (int)(( (axis0_move_time + cameraGetExposure() + cameraGetStabilizationTime()) - (cur_time - prev_time) ) * 1000));
-
-            return 1;   // Error
+            return 1;
         }
     }
 
@@ -93,36 +160,33 @@ int scriptsExecute(char *script, BaseSequentialStream *out)
         return 1;   // Error
     }
 
-    // Local copy(split corrupts string)
-    char script_cpy[128];
-    strcpy(script_cpy, script);
-
-    // Splitting expression
-    int shots = atoi(strtok(script_cpy, "[;]"));
-    char *time_expr = strtok(NULL, "[;]");
-    char *axis0_expr = strtok(NULL, "[;]");
-
+    int shots = solveSteps(script);
     systime_t start_time;
 
     int i;
     for (i = 0; i < shots; i++)
     {
         // Calculating coordinates
-        float cur_time = solveExpr(time_expr, i);
-        float cur_axis0 = solveExpr(axis0_expr, i);
+        float *curCoordinates[COORDINATES_COUNT];
+        solveCoordinates(script, i, curCoordinates, COORDINATES_COUNT);
 
         if (i)  // Not first shot
         {
-            float prev_time  = solveExpr(time_expr, i - 1);
+            float *prevCoordinates[COORDINATES_COUNT];
+            solveCoordinates(script, i - 1, prevCoordinates, COORDINATES_COUNT);
+            float prevTime  = prevCoordinates[0];
 
             // Wait for | shot time + free time = interval - stabilization - move
-            while (chVTGetSystemTime() - start_time < (systime_t)((prev_time + cameraGetExposure()) * CH_CFG_ST_FREQUENCY))
+            while (chVTGetSystemTime() - start_time < (systime_t)((prevTime + cameraGetExposure()) * CH_CFG_ST_FREQUENCY))
             {
                 chThdSleepMilliseconds(SLEEP_INTERVAL);
             }
 
             // Move axis
-            axisSetDestinationPos(AXIS0, cur_axis0);
+            for (int j = 0; j < AXIS_COUNT; j++)
+            {
+                axisSetDestinationPos(j, curCoordinates[j + 1]);
+            }
 
             // Wait for shot
             while (chVTGetSystemTime() - start_time < (systime_t)(cur_time * CH_CFG_ST_FREQUENCY))
@@ -133,12 +197,33 @@ int scriptsExecute(char *script, BaseSequentialStream *out)
         else    // First shot
         {
             // Move axis to start position
-            axisSetDestinationPos(AXIS0, cur_axis0);
+            for (int j = 0; j < AXIS_COUNT; j++)
+            {
+                axisSetDestinationPos(j, curCoordinates[j + 1]);
+            }
 
             // Wait for axis
-            while (axisGetState(AXIS0) == AXIS_STATE_RUN)
+            while (1)
             {
-                chThdSleepMilliseconds(SLEEP_INTERVAL);
+                int isMotion = 0;
+                
+                for (int j = 0; j < AXIS_COUNT; j++)
+                {
+                    if (axisGetState(j) == AXIS_STATE_RUN)
+                    {
+                        isMotion = 1;
+                        break;
+                    }
+                }
+                
+                if (isMotion)
+                {
+                    chThdSleepMilliseconds(SLEEP_INTERVAL);
+                }
+                else
+                {
+                    break;
+                }
             }
 
             // Remember start time
@@ -149,7 +234,12 @@ int scriptsExecute(char *script, BaseSequentialStream *out)
         cameraShoot();
 
         // Write log
-        chprintf(out, "Step: %d, time: %d, axis1: %d\r\n", i, (int)(cur_time * 1000), (int)(cur_axis0 * 1000));
+        chprintf(out, "Step: %d, time: %d", i, ftoi(cur_time));
+        for (int j = 0; j < AXIS_COUNT; j++)
+        {
+            chprintf(out, ", axis%d: %d", j, curCoordinates[j + 1]);
+        }
+        chprintf(out, "\r\n");
     }
 
     return 0;
